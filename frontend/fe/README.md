f0db465bfb8ce53b25145098cd7f2f9ddb8ce16c
CsobothEniko <enikc016@gmail.com>
2022-03-15 20:49:54 +0100

    frontend

---
+++ frontend/fe/README.md
@@ -0,0 +1,34 @@
+# fe
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### buildelés előtt: eslintrc.js
+  overrides: [
+    {
+      files: ['src/views/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
+    },
+  ]
---
+++ frontend/fe/src/views/HomeView.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>{{ title }}</p>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id" :class="{completed:task.completed}">
+        {{ task.name }} - {{ task.due }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+//import { defineComponent } from '@vue/composition-api'
+
+export default {
+  name: "home",
+  components: {},
+  data() {
+    return {
+      title: "Home",
+      tasks: [
+        {
+          id: 1,
+          name: "első",
+          completed: false,
+          due: "2022-03-05",
+        },
+        {
+          id: 2,
+          name: "második",
+          completed: true,
+          due: "2022-03-05",
+        },
+        {
+          id: 3,
+          name: "harmadik",
+          completed: false,
+          due: "2022-03-05",
+        },
+        {
+          id: 4,
+          name: "negyedik",
+          completed: false,
+          due: "2022-03-05",
+        },
+      ],
+    };
+  },
+  computed: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.completed{
+  text-decoration:line-through;
+}
+</style>
